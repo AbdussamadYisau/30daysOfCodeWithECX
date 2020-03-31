3a892bc776c7a5901d02d8c847d03088144d1452
AbdussamadYisau <Sammieyisau@gmail.com>
2020-03-31 09:54:04 +0100

    Fixed this error, but now there's another error: Math domain

--- Day5.py
+++ Day5.py
@@ -6,35 +6,33 @@ import math
 
 
 def faithful(n):
-	if ( n == 1 ): 
-		{
+	if (n == 1): 
 		print("1")
-		}
 
-	elif((math.log(n)/math.log(2)) % 1 == 0):
-		{
+	elif (math.log(n)/math.log(2)) % 1 == 0:
+		
 		power = int((math.log(n)/math.log(2)) /1)
 		retVal = int(math.pow(7,power))
 
 		print(retVal)
-		}
+		
 
 	# Creating a set of dummy variable to handle cases beside when n is 1 or when n is a perfect 2
 	prev = 1
 	nex = 1
 	diff = 0
 
-	while(nex < n):
-		{
+	while nex < n:
+		
 		prev = nex
 		nex = nex * 2
-		}
+		
 
 	diff = n - prev
 
 	print(faithful(diff) + faithful(prev))
 
 
-# faithful(1)
+faithful(8)
 
 
