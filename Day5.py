0994bf82159caa131c61ee614d5843f14747c064
AbdussamadYisau <Sammieyisau@gmail.com>
2020-03-31 10:25:18 +0100

    What was causing the math domain error is because i used print in the function, not return. Fixed now though

--- Day5.py
+++ Day5.py
@@ -7,14 +7,14 @@ import math
 
 def faithful(n):
 	if (n == 1): 
-		print("1")
+		return 1
 
-	elif (math.log(n)/math.log(2)) % 1 == 0:
+	elif ((math.log(n)/math.log(2)) % 1 == 0):
 		
 		power = int((math.log(n)/math.log(2)) /1)
 		retVal = int(math.pow(7,power))
 
-		print(retVal)
+		return(retVal)
 		
 
 	# Creating a set of dummy variable to handle cases beside when n is 1 or when n is a perfect 2
@@ -30,9 +30,18 @@ def faithful(n):
 
 	diff = n - prev
 
-	print(faithful(diff) + faithful(prev))
+	return(faithful(diff) + faithful(prev))
 
 
-faithful(8)
+print(faithful(1))
+print(faithful(2))
+print(faithful(3))
+print(faithful(4))
+print(faithful(5))
+print(faithful(6))
+print(faithful(7))
+print(faithful(8))
+print(faithful(9))
+print(faithful(10))
 
 
